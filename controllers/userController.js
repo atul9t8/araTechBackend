12e99b4e026ced5ebefc07867f9c7185b89329c7
Atul <atudebnath8@yahoo.com>
2022-09-25 04:07:32 +0600

    Second

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const User = require("../models/userModel")
+const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
+
 
 const registration = async(req, res)=>{
     if(req.body.email){
-        const user = new User({
-        email: req.body.email,
-        password: req.body.password
-    })
-
-    User.create(user, (err, data)=>{
-        if(err){
-            res.send(err)
+        if(req.body.password === req.body.cPassword){
+            bcrypt.hash(req.body.password, 10, (err, hash)=>{
+                password = hash;
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                    }
+                )
+                User.create(user, (err, data)=>{
+                    if(err){
+                        res.send(err)
+                    }else{
+                        res.send(data)
+                    }
+                    }
+                )
+            })
         }else{
-            res.send(data)
+            res.send("Password and Confirm password doesn't match!")
         }
-    }) 
+        
     }else{
         res.send("Enter an email address")
     }
+        
 
 }
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
-const sql = require('../controllers/db')
+const sql = require('../controllers/db');
+const jwt = require("jsonwebtoken")
+
 
 const User = function(user){
     this.email = user.email;
@@ -6,14 +8,35 @@ const User = function(user){
 };
 
 User.create = (newUser, result)=>{
-    sql.query("INSERT INTO userinfo SET ?", newUser, (err, res)=>{
+    sql.query("SELECT * FROM userinfo WHERE email = ?", newUser.email, (err, res)=>{
         if(err){
-            console.log(err);
-            result(err, null);
+            console.log("Exixting Email Error: ", err)
             return;
+        }else{
+            if(res.length){
+                console.log(res.length);
+                result("Email Already Exists!")
+            }else{
+                sql.query("INSERT INTO userinfo SET ?", newUser, (err, res)=>{
+                    if(err){
+                        console.log(err);
+                        result(err, null);
+                        return;
+                    }
+                    const token = jwt.sign({id: newUser.id, email: newUser.email}, process.env.OCEAN_BLUE)
+
+                    console.log("created data")
+                    result(token)
+                    }
+                )
+            }
         }
-        console.log("created data")
     })
+
+}
+
+User.checkExistingEmail = (newEmail, result)=>{
+
 }
 
 
